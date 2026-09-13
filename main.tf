7c4823f34d6ee455bb5b6b912bd9ff7fda80850c
vivek <vivekchiguru8@gmail.com>
2026-09-13 17:40:39 +0530

    docker

--- main.tf
+++ main.tf
@@ -1,19 +1,10 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "google" {
   project = "project-10094705-9153-43d5-bb8"
-  region  = "us-central1"
+  region  = "asia-south1"
 }
 
 resource "google_artifact_registry_repository" "my_app_repo" {
-  location      = "us-central1"
+  location      = "asia-south1"
   repository_id = "my-app-repo"
   description   = "Go app docker images"
   format        = "DOCKER"
