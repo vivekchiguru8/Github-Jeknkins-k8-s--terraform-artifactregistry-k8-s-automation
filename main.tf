2a407dfe3e0b26422f3fa46b85a49668fa86238a
vivekchiguru8 <vivekchiguru8@gmail.com>
2026-09-23 17:06:37 +0530

    Add Google Kubernetes Engine cluster resource
    Added a Google Kubernetes Engine cluster resource with initial configuration.

--- main.tf
+++ main.tf
@@ -6,8 +6,19 @@ provider "google" {
 resource "google_artifact_registry_repository" "my_app_repo" {
   location      = "asia-south1"
   repository_id = "my-app-repo"
-  description   = "Go app docker images"
   format        = "DOCKER"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-go-cluster"
+  location = "asia-south1-a"
+  initial_node_count = 2
+  node_config {
+    machine_type    = "e2-medium"
+    disk_size_gb    = 30
+    service_account = "645491051550-compute@developer.gserviceaccount.com"
+    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 }
 
 output "registry_url" {
